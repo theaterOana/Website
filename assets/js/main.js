526829e780f20a21e1bd6f736db95f9d5dd5e3a0
Matthias Bruynooghe <matthias.bruynooghe@student.howest.be>
2018-03-07 08:37:05 +0100

    change to jeugd only

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,27 @@
 
 var init = function () {
 
+pageLocationFunction();
+
+
+
 };
+
+
+pageLocationFunction = function(){
+  var path = window.location.pathname;
+  path = path.split("/").pop().split('.')[0];
+  console.log(path);
+  if (path === "index") {
+    $('#breadcrum').text('Home')
+    $('#home').addClass('active')
+  }
+  else {
+    $('#breadcrum').text(path)
+    $('#'+path).addClass('active')
+  }
+}
+
 
 
 $(document).ready(init);
