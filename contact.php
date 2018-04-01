41fedf550ecdc7ae6176d63b74491b999499c63f
Unknown <matthias.bruynooghe@student.howest.be>
2018-04-01 19:12:53 +0200

    mail send

--- contact.php
+++ contact.php
@@ -18,18 +18,21 @@ require_once("Head.php");
                 <?php
 
 
+                function sendmail()
+                {
+
+                }
+
                 function echoForm()
                 {
-                    echo "   <form class=\"\" action=\"contact.php\" method=\"post\">" .
+                    echo "<form action='#' method='post' ><div><label for=\"naam\">Je naam:</label>" .
+                        "<input type=\"text\" id=\"naam\" name=\"naam\" content='" . (isset($_POST["naam"]) ? $_POST["naam"] : "") . "'></div>" .
 
-                        "<label for=\"naam\">Je naam</label>" .
-                        "<input type=\"text\" id=\"naam\" name=\"naam\" content='" . (isset($_POST["naam"]) ? $_POST["naam"] : "") . "'>" .
+                        "<div><label for=\"email\">Je e-mail adres:</label>" .
+                        "<input type=\"email\" name=\"email\" id=\"email\" content='" . (isset($_POST["email"]) ? $_POST["email"] : "") . "'></div>" .
 
-                        "<label for=\"email\">Je e-mail adres</label>" .
-                        "<input type=\"email\" name=\"email\" id=\"email\" content='" . (isset($_POST["email"]) ? $_POST["email"] : "") . "'>" .
-
-                        "<label for=\"message\">Je bericht:</label>" .
-                        "<input type=\"text\" id=\"message\" name=\"message\" content='" . (isset($_POST["message"]) ? $_POST["message"] : "") . "'>" .
+                        "<div><label for=\"message\">Je bericht:</label>" .
+                        "<textarea id=\"message\" name=\"message\" content=\"" . (isset($_POST["message"]) ? $_POST["message"] : "") . "\"></textarea></div> " .
 
                         "<input type=\"submit\">" .
                         "</form>";
@@ -38,33 +41,32 @@ require_once("Head.php");
                 if (isset($_POST["email"]) && isset($_POST["message"])) {
 
 
-                    $to = "matthias.bruynooghe@gmail.com";
-                    $subject = "Een mail van de pagina van " . $_POST["naam"];
-                    $txt = $_POST["message"];
-                    $headers = "From: " . $_POST["email"];
 
 
-                    $email = test_input($_POST["email"]);
-                    $emailErr = false;
-                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                        $emailErr = true;
-                    }
 
 
-//                    if ($txt == "" || $emailErr) {
-//                        echo "<p>Je moet nog de volgende velden invullen of verbeteren: ";
-//                        echo (isset($_POST["email"]) && $_POST["email"] != "" ? "e-mail " : "") . (isset($_POST["message"]) && $_POST["message"] != "" ? "bericht" : "")."</p>";
-//                        echoForm();
-//
-//
-//                    } else {
+                    if (empty($_POST["message"]) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        echo "<p>Je moet nog de volgende velden invullen of verbeteren: ";
+                        echo (isset($_POST["email"]) && $_POST["email"] != "" ? "e-mail " : "") . (isset($_POST["message"]) && $_POST["message"] != "" ? "bericht" : "") . "</p>";
+                        echoForm();
 
-                        echo "<p>Je mail is verzonden!</p>";
+
+                    } else {
+                        $to = "matthias.bruynooghe@gmail.com";
+                        $subject = "Een mail van de pagina van " . $_POST["naam"];
+                        $txt = $_POST["message"];
+                        $headers = "From: " . $_POST["email"];
+
 
                         mail($to, $subject, $txt, $headers);
 
+                        echo "<p>Je mail is verzonden!</p>";
 
-                   // }
+
+                    }
+
+
+
 
 
                 } else {
