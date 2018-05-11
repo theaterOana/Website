a2abbe7b0f677859e9d8eb89cb58449ae038aee3
Unknown <matthias.bruynooghe@student.howest.be>
2018-05-11 19:31:31 +0200

    service worker active

--- Tail.php
+++ Tail.php
@@ -1,6 +1,17 @@
+ <script type="text/javascript" src="sw.js"></script>
+ <script type="text/javascript">
+ 	if (navigator.serviceWorker.controller) {
+  
+} else {
 
-
-<script type="text/javascript" src="sw.js"></script>
+//Register the ServiceWorker
+  navigator.serviceWorker.register('sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    
+  });
+}
+ </script>
  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script type="text/javascript" src="assets\js\bootstrap.min.js"></script>
  <script type="text/javascript" src="assets/js/main.js">
--- sw.js
+++ sw.js
@@ -1,74 +1,81 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-  '/css/main.css',
-  '/js/main.js'
-];
+//This is the service worker with the Cache-first network
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+        'assets/css/header.css',
+  'assets/css/reset.css',
+  'assets/css/screen.css',
+    'assets/js/bootstrap.min.js',
+      'images/Claudine.jpg',
+  'images/Elien.jpg',
+  'images/Jana.jpg',
+  'images/Jonas.jpg',
+  'images/Logo-Oana.png',
+  'images/Matthias.jpg',
+  'images/Sylke.jpg',
+  'images/browserconfig.xml',
+  'images/geschied/02.jpg',
+  'images/geschied/04.jpg',
+  'images/geschied/05.jpg',
+  'images/oneindigeVerhaal.jpg',
+  'images/ticketwinkel_logo.png',
+    'index.php',
+      'index.php',
+        'Voor_Leden.php',
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+
+  evt.waitUntil(precache().then(function() {
+    console.log('[ServiceWorker] Skip waiting on install');
+      return self.skipWaiting();
+
+  })
   );
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
 
-      // IMPORTANT: Clone the request. A request is a stream and
-      // can only be consumed once. Since we are consuming this
-      // once by cache and once by the browser for fetch, we need
-      // to clone the response.
-      var fetchRequest = event.request.clone();
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+      return self.clients.claim();
 
-      return fetch(fetchRequest).then(
-        function (response) {
-          // Check if we received a valid response
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          // IMPORTANT: Clone the response. A response is a stream
-          // and because we want the browser to consume the response
-          // as well as the cache consuming the response, we need
-          // to clone it so we have two streams.
-          var responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then(function (cache) {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        }
-      );
-    })
-  );
 });
 
-self.addEventListener('activate', function (event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', function(evt) {
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
 });
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cahche to go to the server and get it
+return fetch(request).then(function(response){ return response})
+}
