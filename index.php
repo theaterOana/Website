32ec435a8ddf8d543d1813c8b6184cafb2e373bf
Matthias Bruynooghe <matthias.bruynooghe@student.howest.be>
2018-12-31 10:29:49 +0100

    voorpagina naar de kans*

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-   <?php
+<?php
 
 require_once("Head.php");
  ?>
@@ -6,63 +6,62 @@ require_once("Head.php");
 
 
 
-   <main class="col-md-10 offset-md-1 ">
+<main class="col-md-10 offset-md-1 ">
 
 
-     <div class="row">
+  <div class="row">
 
 
-          <div class=" col-sm-12">
-            <article >
-              <h1>Theater O'ana</h1>
-              <p>Op 10 maart 2018 zag een nieuwe Torhoutse theatervereniging het licht: Theater O’ana. O’ana is afgeleid van het Hawaïaanse woord voor “familie”. Hiermee wil Theater O’ana meer zijn dan enkel een theatervereniging. We willen een hechte groep creëren waar er plaats is voor alle jongeren met dezelfde passie voor theater. </p>
-          <a href="Over_Ons.php" class="leesmeer">lees meer</a>
-            </article>
-          </div>
+    <div class=" col-sm-12">
+      <article>
+        <h1>Theater O'ana</h1>
+        <p>Op 10 maart 2018 zag een nieuwe Torhoutse theatervereniging het licht: Theater O’ana. O’ana is afgeleid van
+          het Hawaïaanse woord voor “familie”. Hiermee wil Theater O’ana meer zijn dan enkel een theatervereniging. We
+          willen een hechte groep creëren waar er plaats is voor alle jongeren met dezelfde passie voor theater. </p>
+        <a href="Over_Ons.php" class="leesmeer">lees meer</a>
+      </article>
+    </div>
 
 
-             <div class=" col-sm-12">
-             <article >
-                 <h1>Het Oneindige Verhaal</h1>
-                 <p> In september ging onze allereerste voorstelling in première. Meer dan 400 mensen volgden Bastiaans fantasiewereld waarin Atréjoe zocht naar een geneesmiddel om Fantasia en de Kleine Keizerin te redden. Kon je niet aanwezig zijn of wil je gewoon de voorstelling opnieuw beleven? Hou dan onze website in de gaten, want binnenkort verschijnen hier een aantal beelden. </p>
-             </article>
-
-         </div>
-
-         <div class=" col-sm-12">
-         <article >
-             <h1>Die Unendlichen Feste</h1>
-             <p>Om onze voorstellingen te realiseren, organiseerden we in mei “Die Unendlichen Feste”. Een waar Duits feest waar men kon genieten van een heerlijke BBQ. Bedankt aan alle helpende handen en aanwezigen!</p>
-         </article>
-
-     </div>
+    <div class=" col-sm-12">
+      <article>
+        <h1>De kans*</h1>
+        <p>Ook in 2019 hebben we allemaal nieuwe voornemens, zoals meer sporten, gezonder eten, beter studeren, meer bij grootouders op bezoek gaan… De kans dat we die voornemens volhouden is echter niet zo groot. We laten ons snel verleiden door een avondje in de zetel, met een zakje chips, kijkend naar de televisie. Dag voornemens! </p>
+        <p>De kans dat er iets onverwachts gebeurt tijdens zo’n avondje in de zetel, is eerder klein. We zijn veilig in ons eigen huis. Of toch niet?! Bij Anaïs thuis gebeuren er vreemde dingen en er vallen zelfs doden! Is het haar verbeelding die op volle toeren draait, of is dit alles realiteit?</p>
+        <p>Geen paniek! Bij “De kans*” worden veiligheidsmaatregelen getroffen, zodat we het toneelstuk veilig op de scène kunnen zetten. (Of je het einde van de voorstelling haalt, is nog onduidelijk.) </p>
+        <p>Kom dus zeker auditie doen! </p>
+        <p>*Dat u deze voorstelling overleeft, is minimaal</p>
+      </article>
 
 
+    </div>
 
 
 
-       </div>
-</div>
-
- </main>
 
 
- <section class="sponsors">
+  </div>
+  </div>
 
- </section>
- <script type="text/javascript">
+</main>
+
+
+<section class="sponsors">
+
+</section>
+<script type="text/javascript">
   if (navigator.serviceWorker.controller) {
 
-} else {
+  } else {
 
-//Register the ServiceWorker
-  navigator.serviceWorker.register('sw.js', {
-    scope: './'
-  })
-}
- </script>
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('sw.js', {
+      scope: './'
+    })
+  }
+</script>
 
- <?php
+<?php
 
  require_once("Tail.php");
   ?>
