7bd4955dbfd06d7ca5c4646541f6e189bb7e4b7e
Matthias Bruynooghe <matthias.bruynooghe@gmail.com>
2019-05-26 10:21:43 +0200

    geschiedenis

--- geschiedenis.php
+++ geschiedenis.php
@@ -4,23 +4,29 @@ require_once("Head.php");
  ?>
 <link rel="stylesheet" type="text/css" href="assets/css/Timeline.css">
 
-  <main class="col-md-10 offset-sm-1 ">
-    <div class="timeline">
-  <div class="container left">
-    <div class="content frame">
-      <h2>Het Oneindige Verhaal</h2>
+<main class="col-md-10 offset-sm-1 ">
+  <div class="timeline">
+  <div class="container right">
+      <div class="content frame">
+        <h2>De Kans* 2019</h2>
 
-      <a href="Producties\Het_Oneindige_Verhaal.php" class="leesmeer">Lees meer</a>
-      <img src="images/geschied/Het_Oneindige_Verhaal-edit.jpg" alt="">
+        <a href="Producties\De_Kans.php" class="leesmeer">Lees meer</a>
+        <img src="images/fleyerDeKans.png" alt="">
+      </div>
+    </div>
+    <div class="container left">
+      <div class="content frame">
+        <h2>Het Oneindige Verhaal  2018</h2>
+
+        <a href="Producties\Het_Oneindige_Verhaal.php" class="leesmeer">Lees meer</a>
+        <img src="images/geschied/Het_Oneindige_Verhaal-edit.jpg" alt="">
+      </div>
     </div>
   </div>
-  
-
-</div>
-  </main>
+</main>
 
 
-  <?php
+<?php
 
   require_once("Tail.php");
    ?>
