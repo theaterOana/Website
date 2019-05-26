cb0012226e8d0bc17c954788c4730953dece0608
Matthias Bruynooghe <matthias.bruynooghe@gmail.com>
2019-05-26 10:10:24 +0200

    Schoonmaken en fleyer De kans* toevoegen.

--- dist/FILE_NAME.js
+++
@@ -1,3 +0,0 @@
-/*! PROJECT_NAME - v0.1.0 - 2018-07-21
-* http://PROJECT_WEBSITE/
-* Copyright (c) 2018 YOUR_NAME; Licensed MIT */
--- index.php
+++ index.php
@@ -11,32 +11,16 @@ require_once("Head.php");
 
   <div class="row">
 
-
-    <div class=" col-sm-12">
-      <article>
-        <h1>EHBBQ (Eerste Hulp bij Barbecue)</h1>
-        <p>Op 27 april is er terug onze barbecue je kan inschrijven via het contactformulier, een mailje naar info@theater-oana.be of via de leden. Bij
-          een mailtje of het contactformulier vermeld je ook met hoe veel mensen je komt.</p>
-
-      </article>
-
-
-    </div>
-
     <div class=" col-sm-12">
       <article>
         <h1>De kans*</h1>
-        <p style="font-weight:bold ">Ook in 2019 hebben we allemaal nieuwe voornemens, zoals meer sporten, gezonder
-          eten, beter studeren, meer bij grootouders op bezoek gaan… De kans dat we die voornemens volhouden is echter
-          niet zo groot. We laten ons snel verleiden door een avondje in de zetel, met een zakje chips, kijkend naar de
-          televisie. Dag voornemens! </p>
         <p>De kans dat er iets onverwachts gebeurt tijdens zo’n avondje in de zetel, is eerder klein. We zijn veilig in
           ons eigen huis. Of toch niet?! Bij Anaïs thuis gebeuren er vreemde dingen en er vallen zelfs doden! Is het
           haar verbeelding die op volle toeren draait, of is dit alles realiteit?</p>
         <p>Geen paniek! Bij “De kans*” worden veiligheidsmaatregelen getroffen, zodat we het toneelstuk veilig op de
           scène kunnen zetten. (Of je het einde van de voorstelling haalt, is nog onduidelijk.) </p>
         <p style="font-size:11">*Dat u deze voorstelling overleeft, is minimaal</p>
-        <img src="/images/DeKansAuditieFlyer.jpg" alt="Auditie Flyer 'De Kans*'">
+        <img src="/images/fleyerDeKans.png" alt="Flyer 'De Kans*' Voorstellingen op 14, 20 en 21 september 2019 in regie van Lotte Cools. Naar de tekst van Tom Bruynooghe.">
 
         <a href="huidige_productie.php" class="leesmeer">lees meer</a>
       </article>
