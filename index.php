077761f65455ca5825b0378d73333272ba6586a5
Matthias Bruynooghe <matthias.bruynooghe@gmail.com>
2019-06-16 00:25:17 +0200

    Added link for ticket sale.

--- index.php
+++ index.php
@@ -11,6 +11,28 @@ require_once($_SERVER['DOCUMENT_ROOT']."/Head.php");
 
   <div class="row">
 
+
+
+  <div class=" col-sm-12">
+      <article>
+        <h1>De kans*</h1>
+        <p>De kans dat er iets onverwachts gebeurt tijdens zo’n avondje in de zetel, is eerder klein. We zijn veilig in
+          ons eigen huis. Of toch niet?! Bij Anaïs thuis gebeuren er vreemde dingen en er vallen zelfs doden! Is het
+          haar verbeelding die op volle toeren draait, of is dit alles realiteit?</p>
+        <p>Geen paniek! Bij “De kans*” worden veiligheidsmaatregelen getroffen, zodat we het toneelstuk veilig op de
+          scène kunnen zetten. (Of je het einde van de voorstelling haalt, is nog onduidelijk.) </p>
+        <p style="font-size:11">*Dat u deze voorstelling overleeft, is minimaal</p>
+
+<p>Tickets zijn beschikbaar op <a href="https://www.ticketwinkel.be/Event/Details/de-kans">Ticketwinkel</a></p>
+        <img src="/images/fleyerDeKans.png" alt="Flyer 'De Kans*' Voorstellingen op 14, 20 en 21 september 2019 in regie van Lotte Cools. Naar de tekst van Tom Bruynooghe.">
+
+      <!--  <a href="huidige_productie.php" class="leesmeer">lees meer</a> -->
+      </article>
+
+
+    </div>
+
+
   <div class=" col-sm-12">
       <article>
         <h1>Tombola</h1>
@@ -37,23 +59,6 @@ require_once($_SERVER['DOCUMENT_ROOT']."/Head.php");
 147
 </p>
      </article>
-
-
-    </div>
-
-    <div class=" col-sm-12">
-      <article>
-        <h1>De kans*</h1>
-        <p>De kans dat er iets onverwachts gebeurt tijdens zo’n avondje in de zetel, is eerder klein. We zijn veilig in
-          ons eigen huis. Of toch niet?! Bij Anaïs thuis gebeuren er vreemde dingen en er vallen zelfs doden! Is het
-          haar verbeelding die op volle toeren draait, of is dit alles realiteit?</p>
-        <p>Geen paniek! Bij “De kans*” worden veiligheidsmaatregelen getroffen, zodat we het toneelstuk veilig op de
-          scène kunnen zetten. (Of je het einde van de voorstelling haalt, is nog onduidelijk.) </p>
-        <p style="font-size:11">*Dat u deze voorstelling overleeft, is minimaal</p>
-        <img src="/images/fleyerDeKans.png" alt="Flyer 'De Kans*' Voorstellingen op 14, 20 en 21 september 2019 in regie van Lotte Cools. Naar de tekst van Tom Bruynooghe.">
-
-      <!--  <a href="huidige_productie.php" class="leesmeer">lees meer</a> -->
-      </article>
 
 
     </div>
