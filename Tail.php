60051bb8ff14cfaaeb6abe0670ed2cadbd75ac26
Matthias Bruynooghe <Matthias.bruynooghe@gmail.com>
2019-06-24 21:45:00 +0200

    Oops, we do need this

--- Tail.php
+++ Tail.php
@@ -1,5 +1,9 @@
  <script type="text/javascript" src="/sw.js"></script>
 
+ <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <script type="text/javascript" src="/assets\js\bootstrap.min.js"></script>
+ </script>
+
 </body>
 
 </html>
