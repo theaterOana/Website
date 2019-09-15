2265fd0997dd8e4ff3c2ecc3ad88996276d6304e
Matthias Bruynooghe <matthias.bruynooghe@student.howest.be>
2019-09-15 20:36:52 +0200

    added pull file

---
+++ pull.php
@@ -0,0 +1,8 @@
+<?php
+
+// Use in the Post-Receive URL section of your GitHub repo.
+
+shell_exec( 'cd /home/theate1q/public_html/ && git reset --hard origin/master && git pull');
+
+
+?>You shouldn't be here...
