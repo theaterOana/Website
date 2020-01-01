a3ff8bf47eddcccb3252ed46875327d25cf45b0b
Elien Van Aerde <elien.vanaerde90@gmail.com>
2020-01-01 16:19:17 +0100

    Prodcution to audition.

--- Producties/Geen_Productie.php
+++ Producties/Geen_Productie.php
@@ -7,7 +7,10 @@ require_once($_SERVER['DOCUMENT_ROOT']."/Head.php");
   <main class="col-md-10 offset-sm-1 ">
     <div class="row">
       <div class="col-md-12 frame">
-          <p>         Na een succesvol theaterseizoen keert Theater O’ana in 2019 terug met een nieuwe productie. Meer informatie hierover kan je begin 2019 op deze pagina vinden!
+          <p>    Na een succesvolle opvoering in 2019 keren we binnekort terug met een vernieuwde concept. <br><br>
+              Hierbij mogen geïntresseerde acteurs en actrices alvast 15 februari 2020 aanduiden in de agenda! <br>
+              Op deze dag organiseren we onze auditie. Deze keer niet voor één productie, nee nee! <br>
+              De auditie van 15 februari is namelijk voor O'ana. Dus niet langer voor 1 productie maar ook voor deze die hierop zullen volgen.
           </p>   
           </div>
    </div>
