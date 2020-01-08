234daba2f02e0525c7e4bc798800c1657c52f779
Elien Van Aerde <elien.vanaerde90@gmail.com>
2020-01-08 16:33:05 +0100

    Other Spelling!

--- Producties/Geen_Productie.php
+++ Producties/Geen_Productie.php
@@ -8,7 +8,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/Head.php");
     <div class="row">
       <div class="col-md-12 frame">
           <p>    Na een succesvolle opvoering in 2019 keren we binnenkort terug met een vernieuwd concept. <br><br>
-              Hierbij mogen geïntresseerde acteurs en actrices alvast 15 februari 2020 aanduiden in de agenda! <br>
+              Hierbij mogen geïnteresseerde acteurs en actrices alvast 15 februari 2020 aanduiden in de agenda! <br>
               Op deze dag organiseren we onze auditie. Deze keer niet voor één productie, nee nee! <br>
               De auditie van 15 februari is namelijk voor Theater O'ana. Dus niet langer voor 1 productie, maar ook voor deze die hierop zullen volgen.
           </p>   
